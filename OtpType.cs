d3d182812b2a300b389528191646e4a6855a9a20
devin <devin@localhost>
2012-11-02 16:55:31 -0600

    comment some things

--- OtpType.cs
+++ OtpType.cs
@@ -4,9 +4,18 @@ using System.Text;
 
 namespace KeeOtp
 {
+    /// <summary>
+    /// The type of One Time Password to generate
+    /// </summary>
     public enum OtpType
     {
+        /// <summary>
+        /// Timed One Time Password
+        /// </summary>
         Totp,
+        /// <summary>
+        /// HMAC One Time Password
+        /// </summary>
         Hotp
     }
 }
